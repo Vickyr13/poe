04309e45c417c201f46c176c8c0be0f9a1e4a579
javier <carlos.menjivar7@catolica.edu.sv>
2024-08-22 16:10:43 -0600

    Vista de de agragar cuarticiones

--- src/main/java/com/example/demo/controller/VentanaEmegerteCocinaController.java
+++ src/main/java/com/example/demo/controller/VentanaEmegerteCocinaController.java
@@ -1,4 +1,30 @@
 package com.example.demo.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+
 public class VentanaEmegerteCocinaController {
+
+
+    @FXML
+    private ComboBox<String> comboxEnsalada;
+
+    @FXML
+    private ComboBox<String> comboxTortia;
+
+    @FXML
+    private ComboBox<String> comboxBebida;
+
+
+    @FXML
+    private void initialize() {
+
+        //se agregan los items
+        comboxEnsalada.getItems().addAll("Panzanella italiana", "Ensalada Waldorf", "Ensalada casual");
+        comboxTortia.getItems().addAll("Tortia frita", "Tortia tostada", "Tortia caliente");
+        comboxBebida.getItems().addAll("Nada", "Agua", "Fresco", "Soda");
+    }
+
+
 }
