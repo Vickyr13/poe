53daf64d415f677ce02cc5759554d22dce305b58
javier <carlos.menjivar7@catolica.edu.sv>
2024-08-24 08:58:28 -0600

    vista termidada de vebtana emegerte

--- src/main/java/com/example/demo/controller/VentanaEmegerteCocinaController.java
+++ src/main/java/com/example/demo/controller/VentanaEmegerteCocinaController.java
@@ -4,28 +4,25 @@ package com.example.demo.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 
-
 public class VentanaEmegerteCocinaController {
-
-
     @FXML
-    private ComboBox<String> comboxEnsalada;
-
+    private ComboBox<String> combo1;
     @FXML
-    private ComboBox<String> comboxTortia;
-
+    private ComboBox<String> combo2;
     @FXML
-    private ComboBox<String> comboxBebida;
-
+    private ComboBox<String> combo3;
+    @FXML
+    private ComboBox<String> combo4;
+    @FXML
+    private ComboBox<String> combo5;
 
     @FXML
     private void initialize() {
-
         //se agregan los items
-        comboxEnsalada.getItems().addAll("Panzanella italiana", "Ensalada Waldorf", "Ensalada casual");
-        comboxTortia.getItems().addAll("Tortia frita", "Tortia tostada", "Tortia caliente");
-        comboxBebida.getItems().addAll("Nada", "Agua", "Fresco", "Soda");
+        combo1.getItems().addAll("items1", "items2", "items3");
+        combo2.getItems().addAll("items1", "items2", "items3");
+        combo3.getItems().addAll("items1", "items2", "items3");
+        combo4.getItems().addAll("items1", "items2", "items3");
+        combo5.getItems().addAll("items1", "items2", "items3");
     }
-
-
 }
