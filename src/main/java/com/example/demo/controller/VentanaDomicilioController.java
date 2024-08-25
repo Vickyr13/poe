d98de2d592f37ac821fb5ddc92b6bbed7d7eb90b
javier <carlos.menjivar7@catolica.edu.sv>
2024-08-25 09:50:14 -0600

    vistas para domicilio y pedido listos

---
+++ src/main/java/com/example/demo/controller/VentanaDomicilioController.java
@@ -0,0 +1,12 @@
+package com.example.demo.controller;
+
+import javafx.fxml.FXML;
+
+public class VentanaDomicilioController {
+
+
+
+
+
+
+}
--- src/main/java/com/example/demo/controller/vistamesaController.java
+++ src/main/java/com/example/demo/controller/vistamesaController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -9,12 +10,15 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 import java.io.IOException;
 import javafx.scene.layout.Pane;
+import javafx.scene.control.Button;
 
 
 public class vistamesaController {
 
     @FXML
     private Pane mesa5;
+    @FXML
+    private Button but_Domicilio;
 
     public void mesa5(MouseEvent mouseEvent) {
 
@@ -32,6 +36,30 @@ public class vistamesaController {
 
             // Hacer que la nueva ventana aparezca encima de la ventana actual
             newStage.initOwner(mesa5.getScene().getWindow());
+            newStage.initModality(Modality.WINDOW_MODAL); // Esto bloquea la ventana principal hasta que la nueva se cierre
+
+            // Mostrar la nueva ventana
+            newStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void but_Domicilio() {
+        try {
+            // Cargar el archivo FXML para la nueva ventana
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/demo/views/ventanaDomicilio.fxml"));
+            Parent root = loader.load();
+
+            // Crear un nuevo Stage
+            Stage newStage = new Stage();
+
+            // Configurar la nueva escena
+            Scene scene = new Scene(root);
+            newStage.setScene(scene);
+
+            // Hacer que la nueva ventana aparezca encima de la ventana actual
+            newStage.initOwner(but_Domicilio.getScene().getWindow());
             newStage.initModality(Modality.WINDOW_MODAL); // Esto bloquea la ventana principal hasta que la nueva se cierre
 
             // Mostrar la nueva ventana
