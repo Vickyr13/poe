74037b95201eadf7e83d6cca8d1d21447d257b93
VinicioD <afteenoon175@gmail.com>
2024-08-28 15:44:32 -0600

    Usiarios diseño

--- src/main/java/com/example/demo/controller/OrdenesController.java
+++ src/main/java/com/example/demo/controller/OrdenesController.java
@@ -1,8 +1,21 @@
 package com.example.demo.controller;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
 
 public class OrdenesController {
 
+    @FXML
+    private ComboBox<String> Cobo1;
 
+    @FXML
+    private ComboBox<String> Combo2;
 
+    @FXML
+    public void initialize() {
+        // Agregar elementos a los ComboBox
+        Cobo1.getItems().addAll("Año", "Mes", "Semana");
+        Combo2.getItems().addAll("Mesero", "Cocina", "Admin");
+    }
 
 }
