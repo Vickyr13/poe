a26a0d85d2472a1faff1383421941b766ec51f25
Brian <brian.chavez@catolica.edu.sv>
2024-08-29 05:53:34 -0600

    Vista Dashboard y AdminVistasPlatos finalizados de diseño, (crea conflicto en el momento de colocar un metodo) no se ha podido realizar el cambio de vistas

--- src/main/java/com/example/demo/controller/AdminVistaPlatosController.java
+++ src/main/java/com/example/demo/controller/AdminVistaPlatosController.java
@@ -1,4 +1,38 @@
 package com.example.demo.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
 public class AdminVistaPlatosController {
+
+    @FXML
+    private AnchorPane CategoriasPAnel;
+
+    @FXML
+    private AnchorPane DashboardPanel;
+
+    @FXML
+    private AnchorPane OrdenesPanel;
+
+    @FXML
+    private AnchorPane ProductosPanel;
+
+    @FXML
+    private AnchorPane UsuariosPanel;
+
+    @FXML
+    private Button btnAgregar;
+
+    @FXML
+    private TableView<String> tableProductos;
+
+    @FXML
+    public void initialize() {
+    tableProductos.getItems().addAll("id_Producto","Nombre","Descripción","Foto");
+    }
+
 }
+
--- src/main/java/com/example/demo/controller/DashboardController.java
+++ src/main/java/com/example/demo/controller/DashboardController.java
@@ -1,4 +1,64 @@
 package com.example.demo.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.ComboBox;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class DashboardController {
+
+    @FXML
+    private AnchorPane CategoriasPAnel;
+
+    @FXML
+    private AnchorPane DashboardPanel;
+
+    @FXML
+    private AnchorPane OrdenesPanel;
+
+    @FXML
+    private AnchorPane PanelORdenesAnulaes;
+
+    @FXML
+    private AnchorPane ProductosPanel;
+
+    @FXML
+    private AnchorPane UsuariosPanel;
+
+    @FXML
+    private ComboBox<String> cboAnos;
+
+    @FXML
+    private ComboBox<String> cboMeses;
+
+    @FXML
+    private AnchorPane panelIngresoAnual;
+
+    @FXML
+    private AnchorPane panelventas;
+
+    @FXML
+    private BarChart<?, ?> tablaBarras;
+
+    @FXML
+    private PieChart tablaDona1;
+
+    @FXML
+    private PieChart tabladona2;
+
+    public void initialize() {
+        cboMeses.getItems().addAll("Enero", "Febrero", "Marzo", "Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre", "Noviembre", "Diciembre");
+        cboAnos.getItems().addAll("2024","2025","2026","2027");
+    }
+
+
+
 }
+
+
+
+
+
