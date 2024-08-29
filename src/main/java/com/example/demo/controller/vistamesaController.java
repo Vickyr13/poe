886c4fb4114c5468a998e5da5f38f7c2a20d3826
javier <carlos.menjivar7@catolica.edu.sv>
2024-08-28 18:54:37 -0600

    Animacion mesa

--- src/main/java/com/example/demo/controller/vistamesaController.java
+++ src/main/java/com/example/demo/controller/vistamesaController.java
@@ -15,58 +15,7 @@ import javafx.scene.control.Button;
 
 public class vistamesaController {
 
-    @FXML
-    private Pane mesa5;
-    @FXML
-    private Button but_Domicilio;
 
-    public void mesa5(MouseEvent mouseEvent) {
 
-        try {
-            // Cargar el archivo FXML para la nueva ventana
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/demo/views/VentanaEmegerteCocina.fxml"));
-            Parent root = loader.load();
-
-            // Crear un nuevo Stage
-            Stage newStage = new Stage();
-
-            // Configurar la nueva escena
-            Scene scene = new Scene(root);
-            newStage.setScene(scene);
-
-            // Hacer que la nueva ventana aparezca encima de la ventana actual
-            newStage.initOwner(mesa5.getScene().getWindow());
-            newStage.initModality(Modality.WINDOW_MODAL); // Esto bloquea la ventana principal hasta que la nueva se cierre
-
-            // Mostrar la nueva ventana
-            newStage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void but_Domicilio() {
-        try {
-            // Cargar el archivo FXML para la nueva ventana
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/demo/views/ventanaDomicilio.fxml"));
-            Parent root = loader.load();
-
-            // Crear un nuevo Stage
-            Stage newStage = new Stage();
-
-            // Configurar la nueva escena
-            Scene scene = new Scene(root);
-            newStage.setScene(scene);
-
-            // Hacer que la nueva ventana aparezca encima de la ventana actual
-            newStage.initOwner(but_Domicilio.getScene().getWindow());
-            newStage.initModality(Modality.WINDOW_MODAL); // Esto bloquea la ventana principal hasta que la nueva se cierre
-
-            // Mostrar la nueva ventana
-            newStage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
 
