671e8dbb7fed35891874d065e3b15670ac08d295
Brian <brian.chavez@catolica.edu.sv>
2024-09-10 05:05:47 -0600

    Conexión a AdminCategorias.fxml Completo, faltan agregar formularios y enviarlos a la BDD

---
+++ src/main/java/com/example/demo/controller/AdminCategoriasController.java
@@ -0,0 +1,73 @@
+package com.example.demo.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AdminCategoriasController {
+
+    @FXML
+    private AnchorPane CategoriasPAnel;
+
+    @FXML
+    private AnchorPane DashboardPanel;
+
+    @FXML
+    private AnchorPane OrdenesPanel;
+
+    @FXML
+    private AnchorPane ProductosPanel;
+
+    @FXML
+    private AnchorPane UsuariosPanel;
+
+    @FXML
+    private Button btnAgregar;
+
+    @FXML
+    void ClickCategorias(MouseEvent event) {
+        CambiarVista("AdminCategorias");
+    }
+
+    @FXML
+    void ClickDashboard(MouseEvent event) {
+        CambiarVista("AdminDashboard");
+    }
+
+    @FXML
+    void ClickOrdenes(MouseEvent event) {
+        CambiarVista("AdminOrdenes");
+    }
+
+    @FXML
+    void ClickProductos(MouseEvent event) {
+        CambiarVista("AdminProductos");
+    }
+
+    @FXML
+    void ClickUsuarios(MouseEvent event) {
+        CambiarVista("AdminUsuarios");
+    }
+
+    public void CambiarVista(String Direccion){
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/demo/views/"+Direccion+".fxml"));
+            Parent root = loader.load();
+
+            Stage stage = (Stage) DashboardPanel.getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
