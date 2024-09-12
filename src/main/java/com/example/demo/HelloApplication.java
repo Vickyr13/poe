24af6b76579e0acaee5a6dcaeca1c97ff5311051
javier <carlos.menjivar7@catolica.edu.sv>
2024-09-12 14:26:56 -0600

    modifcar codigo

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -12,9 +13,14 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/demo/views/Mesero/login-mesero.fxml"));
         //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        Scene scene = new Scene(fxmlLoader.load()); // Crear la escena sin especificar tamaño
         stage.setTitle("Hello!");
+
+        // Quitar la decoración de la ventana
+        stage.initStyle(StageStyle.UNDECORATED);
+
         stage.setScene(scene);
+        stage.sizeToScene(); // Ajusta el tamaño de la ventana para adaptarse a la escena
         stage.show();
     }
 
