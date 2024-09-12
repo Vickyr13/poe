ecd91c06263df9276cf77fa67ae432373be5692d
Vickyr13 <estefanyarami1997@gmail.com>
2024-09-12 10:35:38 -0600

    Login Admin terminado

--- src/main/java/com/example/demo/HelloApplication.java
+++ src/main/java/com/example/demo/HelloApplication.java
@@ -4,17 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/demo/views/Admin/AdminProductos.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/demo/views/Admin/hello-view.fxml"));
         //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        Scene scene = new Scene(fxmlLoader.load()); // Sin especificar el tamaño
+        // Quitar la decoración de la ventana
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.sizeToScene();
         stage.show();
     }
 
