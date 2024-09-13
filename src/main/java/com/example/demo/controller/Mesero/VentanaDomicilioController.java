6f031b884deee0598129757a5b8dc92d951ab599
javier <carlos.menjivar7@catolica.edu.sv>
2024-09-13 17:56:17 -0600

    modificar la vista de ventanaEmegerteCocina.fxml

--- src/main/java/com/example/demo/controller/Mesero/VentanaDomicilioController.java
+++ src/main/java/com/example/demo/controller/Mesero/VentanaDomicilioController.java
@@ -3,9 +3,11 @@ package com.example.demo.controller.Mesero;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Spinner;
 import javafx.stage.Stage;
 
 public class VentanaDomicilioController {
+
     @FXML
     private Button atras;
     public void but_atras(ActionEvent actionEvent) {
--- src/main/java/com/example/demo/controller/Mesero/VentanaEmegerteCocinaController.java
+++ src/main/java/com/example/demo/controller/Mesero/VentanaEmegerteCocinaController.java
@@ -7,6 +7,9 @@ import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 public class VentanaEmegerteCocinaController {
+    public Spinner candidaPlato;
+    public Label nombrePlato;
+    public Button but_continuar;
     @FXML
     private ComboBox<String> combo1;
     @FXML
@@ -18,24 +21,29 @@ public class VentanaEmegerteCocinaController {
     @FXML
     private ComboBox<String> combo5;
     @FXML
-    private Button but_agregar;
+    private Button but_cancelar;
 
     @FXML
     private void initialize() {
         //se agregan los items
-        combo1.getItems().addAll("items1", "items2", "items3");
-        combo2.getItems().addAll("items1", "items2", "items3");
-        combo3.getItems().addAll("items1", "items2", "items3");
-        combo4.getItems().addAll("items1", "items2", "items3");
-        combo5.getItems().addAll("items1", "items2", "items3");
+//        combo1.getItems().addAll("items1", "items2", "items3");
+//        combo2.getItems().addAll("items1", "items2", "items3");
+//        combo3.getItems().addAll("items1", "items2", "items3");
+//        combo4.getItems().addAll("items1", "items2", "items3");
+//        combo5.getItems().addAll("items1", "items2", "items3");
+
+    SpinnerValueFactory<Integer> valor = new SpinnerValueFactory.IntegerSpinnerValueFactory(1,10,1);
+    candidaPlato.setValueFactory(valor);
+
     }
 
-    public void but_agregar(ActionEvent event) {
 
 
+    public void but_cancelar(ActionEvent actionEvent) {
 
         // Cierra la ventana actual (emergente)
-        Stage stage = (Stage) but_agregar.getScene().getWindow();
+        Stage stage = (Stage) but_cancelar.getScene().getWindow();
         stage.close();
+
     }
 }
