84330975982afbccc18523d8b28fb891028cc8a5
javier <carlos.menjivar7@catolica.edu.sv>
2024-09-17 20:10:45 -0600

    Conectar todas las vistas, solo que no se puede retrisecer

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,4 +1,52 @@
 package com.example.demo.controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
 public class HelloController {
+
+    @FXML
+    public Button but_IniciarSesion;
+
+    @FXML
+    public PasswordField tex_contra;
+
+    @FXML
+    public TextField tex_usuario;
+
+
+    public void but_IniciarSesion(ActionEvent event) {
+        String usuarioIngresado = tex_usuario.getText();
+        String contraseñaIngresada = tex_contra.getText();
+
+        if (usuarioIngresado.equals("root") && contraseñaIngresada.equals("123")) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/demo/views/Admin/AdminDashboard.fxml"));
+                Parent root = loader.load();
+
+                Stage stage = (Stage) but_IniciarSesion.getScene().getWindow();
+                Scene scene = new Scene(root);
+
+                stage.setScene(scene);
+                stage.setResizable(false);
+                stage.centerOnScreen();
+                stage.show();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.out.println("Usuario o contraseña incorrectos.");
+        }
+    }
 }
