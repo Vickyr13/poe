e916d38aaede43ed73fde4506b07f315acfee49f
javier <carlos.menjivar7@catolica.edu.sv>
2024-09-27 14:46:16 -0600

    Login de la parter de admin ya verifica la contraseña des de la base de datos (user root, contra 123)

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.database.conneection;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -13,6 +14,10 @@ import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class HelloController {
 
@@ -30,7 +35,15 @@ public class HelloController {
         String usuarioIngresado = tex_usuario.getText();
         String contraseñaIngresada = tex_contra.getText();
 
-        if (usuarioIngresado.equals("root") && contraseñaIngresada.equals("123")) {
+        // Validación de campos vacíos
+        if (usuarioIngresado.isEmpty() || contraseñaIngresada.isEmpty()) {
+            System.out.println("Por favor ingresa usuario y contraseña");
+            return;
+        }
+
+
+
+        if (validar(usuarioIngresado, contraseñaIngresada)) {
             try {
                 FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/demo/views/Admin/AdminDashboard.fxml"));
                 Parent root = loader.load();
@@ -49,4 +62,29 @@ public class HelloController {
             System.out.println("Usuario o contraseña incorrectos.");
         }
     }
+
+
+    private boolean validar(String user, String password){
+        //Aquí se haría la validación de contraseña
+        Connection conn = conneection.getConnection();
+        String sql = "SELECT * FROM loggeo WHERE usename = ? AND pass = ?";
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, user);
+            stmt.setString(2, password);
+
+            ResultSet resultSet = stmt.executeQuery();
+            return resultSet.next();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
+
+
+
+
 }
