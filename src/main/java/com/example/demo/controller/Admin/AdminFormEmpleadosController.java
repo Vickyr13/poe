d4c513566424d7ce4b332ee9433a7773b1440a79
javier <carlos.menjivar7@catolica.edu.sv>
2024-10-03 09:53:39 -0600

    validacion de campos de para agregar enpleados

--- src/main/java/com/example/demo/controller/Admin/AdminFormEmpleadosController.java
+++ src/main/java/com/example/demo/controller/Admin/AdminFormEmpleadosController.java
@@ -64,64 +64,74 @@ public class AdminFormEmpleadosController {
 
     }
 
-
-
-
     public void GuardarEmpleado() {
 
-
-        //Crear el objeto empleadoDAO
-        EmpleadoDAO querys = new EmpleadoDAO();
-        TelefonoDAO queryTel = new TelefonoDAO();
-        DireccionDAO queryDire = new DireccionDAO();
-
-        //obtener los valores de los campos del formulario y otros controladores
-        String nombre = txtNombre.getText();
-        String apellido = txtApellido.getText();
-        String dui = txtDUI.getText();
-        String telefono = txtTelefono.getText();
-        String email = txtEmail.getText();
-        String direccion_ingresada = txtDireccion.getText();
-        LocalDate contratacion = PICKERContratacion.getValue();
-        int rol = 1;
-        int estado = 0;
-        Date contratacionDate = Date.valueOf(contratacion);
+        validarCampos();
 
 
-        if (RdB_Mesero.isSelected()) {
-            rol = 1;
-        } else if (RdB_Cocinero.isSelected()) {
-            rol = 2;
-        } else if (RdB_Repartidor.isSelected()) {
-            rol = 4;
+        if (validarCampos()) {
+           // GuardarEmpleado();
+            JOptionPane.showMessageDialog(null, "\"Empleado ingresado exitosamente");
+
+
+            //Crear el objeto empleadoDAO
+            EmpleadoDAO querys = new EmpleadoDAO();
+            TelefonoDAO queryTel = new TelefonoDAO();
+            DireccionDAO queryDire = new DireccionDAO();
+
+            //obtener los valores de los campos del formulario y otros controladores
+            String nombre = txtNombre.getText();
+            String apellido = txtApellido.getText();
+            String dui = txtDUI.getText();
+            String telefono = txtTelefono.getText();
+            String email = txtEmail.getText();
+            String direccion_ingresada = txtDireccion.getText();
+            LocalDate contratacion = PICKERContratacion.getValue();
+            int rol = 1;
+            int estado = 0;
+            Date contratacionDate = Date.valueOf(contratacion);
+
+
+            if (RdB_Mesero.isSelected()) {
+                rol = 1;
+            } else if (RdB_Cocinero.isSelected()) {
+                rol = 2;
+            } else if (RdB_Repartidor.isSelected()) {
+                rol = 4;
+            }
+
+            if (RbtnActivo.isSelected()) {
+                estado = 1;
+
+            } else if (RbtnInactivo.isSelected()) {
+                estado = 0;
+            }
+
+
+            //Agregar el empleado a la base de datos o mostrar un mensaje de error
+            Telefono telefono1 = new Telefono("123456789", telefono);
+            Direccion direccion1 = new Direccion(direccion_ingresada);
+
+
+            String pin = generateKey();
+            Empleado empleado = new Empleado(nombre, apellido, dui, email, direccion1.getId_direccion(), telefono1.getId_telefono(), contratacionDate, rol, estado, pin);
+
+            try {
+                queryDire.insertDireccion(direccion1);
+                queryTel.insertTelefono(telefono1);
+                querys.insertarEmpleado(empleado);
+                JOptionPane.showMessageDialog(null, "La contraseña de empleados es: " + pin);
+            } catch (Exception e) {
+                System.out.println(direccion1.getId_direccion());
+                System.out.println(telefono1.getId_telefono());
+                System.out.println("Error al insertar empleados " + e.getMessage());
+            }
+            ;
         }
 
-        if (RbtnActivo.isSelected()) {
-            estado = 1;
-
-
-        } else if (RbtnInactivo.isSelected()) {
-            estado = 0;
+        else {
+            JOptionPane.showMessageDialog(null, "Por favor completa todos los campos antes de agregar.");
         }
-
-
-        //Agregar el empleado a la base de datos o mostrar un mensaje de error
-        Telefono telefono1 = new Telefono("123456789", telefono);
-        Direccion direccion1 = new Direccion(direccion_ingresada);
-
-        String pin = generateKey();
-        Empleado empleado = new Empleado(nombre, apellido, dui, email, 2, 2, contratacionDate, rol, estado, pin);
-
-        try {
-            queryDire.insertDireccion(direccion1);
-            queryTel.insertTelefono(telefono1);
-            querys.insertarEmpleado(empleado);
-            JOptionPane.showMessageDialog(null,"La contraseña de empleados es: " + pin);
-        }catch(Exception e){
-            System.out.println(direccion1.getId_direccion());
-            System.out.println(telefono1.getId_telefono());
-            System.out.println("Error al insertar empleados "+e.getMessage());
-        };
 
     }
 
@@ -163,4 +173,51 @@ public class AdminFormEmpleadosController {
 
         return pin.toString();
     }
+
+
+
+
+
+
+
+    // Validaciones de campos vacíos
+
+    public boolean validarCampos() {
+        String nombre = txtNombre.getText();
+        String apellido = txtApellido.getText();
+        String dui = txtDUI.getText();
+        String telefono = txtTelefono.getText();
+        String email = txtEmail.getText();
+
+        ToggleGroup grupoRadio = new ToggleGroup();
+        RbtnInactivo.setToggleGroup(grupoRadio);
+        RbtnActivo.setToggleGroup(grupoRadio);
+
+        ToggleGroup grupoRadio1 = new ToggleGroup();
+        RdB_Mesero.setToggleGroup(grupoRadio1);
+        RdB_Cocinero.setToggleGroup(grupoRadio1);
+        RdB_Repartidor.setToggleGroup(grupoRadio1);
+        RadioButton seleccionado = (RadioButton) grupoRadio.getSelectedToggle();
+
+        if (nombre.isEmpty() || apellido.isEmpty() || dui.isEmpty() || seleccionado == null || telefono.isEmpty() || email.isEmpty()) {
+            System.out.println("Por favor, completa todos los campos antes de agregar.");
+            return false; // Campos no válidos
+        } else {
+            return true; // Campos válidos
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
